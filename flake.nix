4d7afb484655b6bda19234818b10cc7303cd627b
Nicholas Reid <njreid@gmail.com>
2026-03-22 13:21:57 -0700

    flake.nix: set real vendorHash for buildGoModule

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         pname   = "vivary";
         version = "0.1.0-dev";
         src     = lib.cleanSource ./.;
-        vendorHash = lib.fakeHash;
+        vendorHash = "sha256-NYcKSTWT2VtDfJNIZiZbELjW8nNRD3SRig46OW+35NQ=";
         subPackages = [
           "cmd/keeperd"
           "cmd/vivary"
